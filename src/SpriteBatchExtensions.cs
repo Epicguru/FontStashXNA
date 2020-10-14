7de7186eb22d49bd39918ebf11a24415bcb6d323
Roman Shapiro <roman.shapiro@grapecity.com>
2020-10-15 01:36:22 +0700

    Moved CharacterSpacing and LineSpacing back to FontSystem parameters

--- src/SpriteBatchExtensions.cs
+++ src/SpriteBatchExtensions.cs
@@ -7,38 +7,38 @@ namespace FontStashSharp
 	public static class SpriteBatchExtensions
 	{
 		public static float DrawString(this SpriteBatch batch, DynamicSpriteFont font, string text, Vector2 pos, Color color,
-			int characterSpacing = 0, int lineSpacing = 0, float scaleX = 1.0f, float scaleY = 1.0f, float depth = 0.0f)
+			float scaleX = 1.0f, float scaleY = 1.0f, float depth = 0.0f)
 		{
 			var renderer = SpriteBatchRenderer.Instance;
 			renderer.Batch = batch;
-			return font.DrawText(renderer, pos.X, pos.Y, text, color, characterSpacing, lineSpacing, scaleX, scaleY, depth);
+			return font.DrawText(renderer, pos.X, pos.Y, text, color, scaleX, scaleY, depth);
 		}
 
 		public static float DrawString(this SpriteBatch batch, DynamicSpriteFont font, string text, Vector2 pos, Color[] colors,
-			int characterSpacing = 0, int lineSpacing = 0, float scaleX = 1.0f, float scaleY = 1.0f, float depth = 0.0f)
+			float scaleX = 1.0f, float scaleY = 1.0f, float depth = 0.0f)
 		{
 			var renderer = SpriteBatchRenderer.Instance;
 			renderer.Batch = batch;
 
-			return font.DrawText(renderer, pos.X, pos.Y, text, colors, characterSpacing, lineSpacing, scaleX, scaleY, depth);
+			return font.DrawText(renderer, pos.X, pos.Y, text, colors, scaleX, scaleY, depth);
 		}
 
 		public static float DrawString(this SpriteBatch batch, DynamicSpriteFont font, StringBuilder text, Vector2 pos, Color color,
-			int characterSpacing = 0, int lineSpacing = 0, float scaleX = 1.0f, float scaleY = 1.0f, float depth = 0.0f)
+			float scaleX = 1.0f, float scaleY = 1.0f, float depth = 0.0f)
 		{
 			var renderer = SpriteBatchRenderer.Instance;
 			renderer.Batch = batch;
 
-			return font.DrawText(renderer, pos.X, pos.Y, text, color, characterSpacing, lineSpacing, scaleX, scaleY, depth);
+			return font.DrawText(renderer, pos.X, pos.Y, text, color, scaleX, scaleY, depth);
 		}
 
 		public static float DrawString(this SpriteBatch batch, DynamicSpriteFont font, StringBuilder text, Vector2 pos, Color[] colors,
-			int characterSpacing = 0, int lineSpacing = 0, float scaleX = 1.0f, float scaleY = 1.0f, float depth = 0.0f)
+			float scaleX = 1.0f, float scaleY = 1.0f, float depth = 0.0f)
 		{
 			var renderer = SpriteBatchRenderer.Instance;
 			renderer.Batch = batch;
 
-			return font.DrawText(renderer, pos.X, pos.Y, text, colors, characterSpacing, lineSpacing, scaleX, scaleY, depth);
+			return font.DrawText(renderer, pos.X, pos.Y, text, colors, scaleX, scaleY, depth);
 		}
 	}
 }
